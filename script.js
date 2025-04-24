d469ad12b8ef0b68e25a15403a247fa3b84fb62f
RvRamzes <RvRamzes@gmail.com>
2025-04-24 23:29:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ function createDayCell(day) {
   cell.onclick = () => {
     const key = `${currentYear}-${currentMonth}-${day}`;
     const current = daySelections[key] || 0;
-    if (current === 0) daySelections[key] = 8;
-    else if (current === 8) daySelections[key] = 11;
+    if (current === 0) daySelections[key] = 10;
+    else if (current === 8) daySelections[key] = 8;
     else delete daySelections[key];
 
 // ⬇️ Ось цей рядок зберігає все
